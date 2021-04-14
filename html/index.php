4e886028ba73f3ace099da85ef9ea2de2e96d4a8
tmaslikova <55040075+tmaslikova@users.noreply.github.com>
2021-04-14 02:49:50 -0400

    Fix quoting to be nicer, and tweak some html bugs

--- html/index.php
+++ html/index.php
@@ -11,33 +11,33 @@ include $_SERVER['DOCUMENT_ROOT'].'/include/header.php';
     <div class="container">
       <div class="card-columns mb-3 text-center">
         <?php
-            $query = mysqli_query($dbconnect, "SELECT 
-					e.EventID as \"EventID\",
-					e.Category as \"EventCategory\",
-					e.Name as \"EventName\",
-					e.ContactName as \"Coordinator\",
-					e.ContactPhoneNumber as \"CoordinatorNumber\",
-					e.ContactEmailAddr as \"CoordinatorEmail\",
-					e.AddressDesc as \"Where\",
-					e.Scheduled as \"When\",
-					u.Name as \"University\",
-					ro.Name as \"Org\"
+            $query = mysqli_query($dbconn, 'SELECT 
+					e.EventID as "EventID",
+					e.Category as "EventCategory",
+					e.Name as "EventName",
+					e.ContactName as "Coordinator",
+					e.ContactPhoneNumber as "CoordinatorNumber",
+					e.ContactEmailAddr as "CoordinatorEmail",
+					e.AddressDesc as "Where",
+					e.Scheduled as "When",
+					u.Name as "University",
+					ro.Name as "Org"
 				FROM SchoolEventApp.Events e
 				JOIN University u on u.UniversityID = e.UniversityID
 				LEFT JOIN RStudentOrg ro on ro.OrgID = e.OrgID 
-				WHERE e.EventVisibility = 'Public' AND e.Published = 1;")
-                or die (mysqli_error($dbconnect));
+				WHERE e.EventVisibility = "Public" AND e.Published = 1;')
+                or die (mysqli_error($dbconn));
 
             while ($row = mysqli_fetch_array($query)) {
 				
                 echo '<div class="card shadow-sm">
                         <div class="card-header">
-                          <h4 class="my-0 font-weight-normal">'.$row['EventName'].'</h4>
+                          <h4 class="my-0 font-weight-normal">Organized at '.$row['University'].'</h4>
                         </div>
                         <div class="card-body">
-                          <h1 class="card-title pricing-card-title">Organized at '.$row['University'].'</h1>
-						  '.$row['Where'].' at '$row['When']'
-                          <a href="mailto:'.$row['CoordinatorEmail'].'" class="btn btn-lg btn-block btn-outline-primary">Contact '.$row['Coordinator'].'!</button>
+                          <h1 class="card-title pricing-card-title">'.$row['EventName'].'</h1>
+						  '.$row['Where'].' at '.$row['When'].'
+                          <a href="mailto:'.$row['CoordinatorEmail'].'" class="btn btn-lg btn-block btn-outline-primary">Contact '.$row['Coordinator'].'!</a>
                         </div>
                       </div>
                       ';
