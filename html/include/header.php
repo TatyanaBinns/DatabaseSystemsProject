fbf8057a2a6f932490d9818542161128fc70766d
tmaslikova <55040075+tmaslikova@users.noreply.github.com>
2021-04-14 01:37:21 -0400

    Merge navbar stuff into the header

--- html/include/header.php
+++ html/include/header.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include $_SERVER['DOCUMENT_ROOT'].'/include/sql.php';
 ?>
 <!doctype html>
@@ -14,3 +15,25 @@ include $_SERVER['DOCUMENT_ROOT'].'/include/sql.php';
     <title><?php echo $title ?></title>
   </head>
   <body>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+      <nav class="my-0 mr-md-auto font-weight-normal">
+        <a class="p-2 text-dark" href="#">Home Page</a>
+        <a class="p-2 text-dark" href="#">Events</a>
+      </nav>
+      <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="#">My Dashboard</a>
+        <a class="p-2 text-dark" href="#">My Profile</a>
+      </nav>
+	  <?php
+          if (isset($_SESSION["fname"]))
+          {
+            echo "<a class='btn btn-outline-primary' href='profile.php'>Profile</a>";
+            echo "<a class='btn btn-outline-primary' href='logout.inc.php'>Logout</a>";
+          }
+          else 
+          {
+            echo "<a class='btn btn-outline-primary' href='register.php'>Register</a>";
+            echo "<a class='btn btn-outline-primary' href='login.php'>Log in</a>";
+          }
+        ?>
+    </div>
