44f92d682f5339c8af90bcb96e223a1112e956fd
tmaslikova <55040075+tmaslikova@users.noreply.github.com>
2021-04-14 00:41:23 -0400

    Update database name to use the SchoolEventApp database
    Instead of using the test database, we now want it to point at the final database we're gonna use

--- html/include/sql.php
+++ html/include/sql.php
@@ -2,7 +2,7 @@
 $hostname = "localhost";
 $username = "webuser";
 $password = "e3p6ihg93tgj5u97jhcghlkmgrti90bo";
-$db = "exampledb";
+$db = "SchoolEventApp";
 $dbconnect=mysqli_connect($hostname,$username,$password,$db);
 if ($dbconnect->connect_error) {
   die("Database connection failed: " . $dbconnect->connect_error);
