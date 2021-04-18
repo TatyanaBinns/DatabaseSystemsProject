3ed2dfa2f0b8cbe660f2b229bf1c4597c3e20896
tmaslikova <55040075+tmaslikova@users.noreply.github.com>
2021-04-17 21:42:20 -0400

    Add group footer across all pages

--- html/include/footer.php
+++ html/include/footer.php
@@ -1,4 +1,14 @@
 
+	<div class="container text-center">
+	  <footer class="pt-4 my-md-5 pt-md-5 border-top">
+        <div class="row">
+          <div class="col-12 col-md">
+            <h5>COP 4710 Term Project :: Spring 2021</h5>
+			<div>Created by Aisha Hnainia, Tatyana Maslikova, and Michael Tsang</div>
+          </div>
+        </div>
+      </footer>
+	</div>
     <script src="/lib/jquery-3.6.0.min.js"></script>
     <script src="/lib/bootstrap.bundle.min.js"></script>
   </body>
