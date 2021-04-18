2731fbbc5e3e454587748cb94a2703e457d6b424
tmaslikova <55040075+tmaslikova@users.noreply.github.com>
2021-04-17 21:44:44 -0400

    Do lots of tweaking to the navigation bar

--- html/include/header.php
+++ html/include/header.php
@@ -11,14 +11,35 @@ include $_SERVER['DOCUMENT_ROOT'].'/include/sql.php';
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="/lib/bootstrap.min.css">
     <meta name="Description" content="<?php echo $descr ?>">
-
+	<style>
+		.sea-nav-link{
+			padding: .5rem;
+			font-weight: 700;
+			background-color: transparent;
+			border-bottom: .25rem solid transparent;
+			color: #808080;
+		}
+		.sea-nav-link:hover{
+			border-bottom-color: rgba(100, 100, 100, .25);
+			color: #555;
+			text-decoration: none;
+		}
+		.sea-nav-active{
+			color: #000;
+			border-bottom-color: rgba(255, 255, 255, .75);
+		}
+		.sea-nav-active:hover{
+			color: #555;
+			border-bottom-color: rgba(100, 100, 100, .25);
+		}
+	</style>
     <title><?php echo $title ?></title>
   </head>
   <body>
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
       <nav class="my-0 mr-md-auto font-weight-normal">
-        <a class="p-2 text-dark" href="#">Home Page</a>
-        <a class="p-2 text-dark" href="#">Events</a>
+        <a class="sea-nav-link <?php if ($navitem == "homepage") echo "active";?>" href="index.php">Home Page</a>
+        <a class="sea-nav-link <?php if ($navitem == "faq") echo "active";?>" href="faq.php">FAQ</a>
       </nav>
 	  <div class="btn-group mr-2">
 	  <?php
