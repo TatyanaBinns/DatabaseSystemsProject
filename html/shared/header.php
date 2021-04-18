de2e78c4e8f932633e09b2352198a178efb04bc3
tmaslikova <55040075+tmaslikova@users.noreply.github.com>
2021-04-18 02:17:28 -0400

    Add login-sensitive navigation options
    Basically based on the role(s) the user has, offer certain navigation options.

--- html/shared/header.php
+++ html/shared/header.php
@@ -9,6 +9,51 @@ function printError($message, $type){
 			</button>
 		  </div>';
 }
+function getName($conn){
+	if(!isset($_SESSION["userid"]))
+		return "";
+	$sql = "SELECT FirstName,LastName FROM Users WHERE UserID = ?;";
+
+    // prevents the user from entering malicious code into the registration form
+    // prevents code injection
+    $stmt = mysqli_stmt_init($conn); // this makes code more secure
+
+    // check for any mistakes before running sql code 
+    if (!mysqli_stmt_prepare($stmt, $sql))
+    {
+        header("location: register.php?error=stmtfailed");
+        exit();
+    }
+    
+    mysqli_stmt_bind_param($stmt, "i", $_SESSION["userid"]);
+
+    mysqli_stmt_execute($stmt);
+
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    // if theres is info in the data base with this email grab data 
+    // assigns data to var row
+	$row = mysqli_fetch_assoc($resultData);
+	
+    mysqli_stmt_close($stmt);
+    
+	return $row["FirstName"]." ".$row["LastName"];
+}
+function hasRole($conn, $role){
+	$stmt = mysqli_stmt_init($conn); // this makes code more secure
+	// check for any mistakes before running sql code 
+	if (mysqli_stmt_prepare($stmt, 
+	       "SELECT IF(COUNT(RoleType) > 0, 'true', 'false') AS hasRole
+			FROM Roles r
+			WHERE r.UserID = ?
+			AND r.RoleType = ?;")){
+		mysqli_stmt_bind_param($stmt, "is", $_SESSION["userid"], $role);
+		mysqli_stmt_execute($stmt);
+		$row = mysqli_fetch_assoc(mysqli_stmt_get_result($stmt));
+		mysqli_stmt_close($stmt);
+		return $row["hasRole"] == "true";
+	}
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -28,18 +73,35 @@ function printError($message, $type){
         <a class="sea-nav-link <?php if ($navitem == "homepage") echo "active";?>" href="index.php">Home Page</a>
         <a class="sea-nav-link <?php if ($navitem == "faq") echo "active";?>" href="faq.php">FAQ</a>
       </nav>
-	  <div class="btn-group mr-2">
+	  <?php
+		if(isset($_SESSION["fname"])){
+			echo '<div class="sea-username">'.getName($dbconn)."</div>";
+		}
+	  ?>
 	  <?php
           if (isset($_SESSION["fname"]))
           {
-            echo "<a class='btn btn-outline-secondary sea-enboldener' href='profile.php'>Profile</a>";
+			echo '<div class="btn-group mr-2">';
+			if(hasRole($dbconn, "Admin"))
+				echo "<a class='btn btn-outline-secondary sea-enboldener' href='create_event.php'>Create Event</a>";
+			if(hasRole($dbconn, "SuperAdmin"))
+				echo "<a class='btn btn-outline-secondary sea-enboldener' href='create_university.php'>Create University</a>";
+			if(hasRole($dbconn, "Student"))
+				echo "<a class='btn btn-outline-secondary sea-enboldener' href='create_rso.php'>Create Student Org</a>";
+			echo '</div>';
+			
+			echo '<div class="btn-group mr-2">';
+			if(hasRole($dbconn, "ApplicationAdmin"))
+				echo "<a class='btn btn-outline-secondary sea-enboldener' href='manage_users.php'>Manage Users</a>";
             echo "<a class='btn btn-outline-secondary sea-enboldener' href='logout.form.php'>Logout</a>";
+			echo '</div>';
           }
           else 
           {
+			echo '<div class="btn-group mr-2">';
             echo "<a class='btn btn-outline-secondary sea-enboldener ' href='register.php'>Register</a>";
             echo "<a class='btn btn-outline-secondary sea-enboldener' href='login.php'>Login</a>";
+			echo '</div>';
           }
         ?>
-	  </div>
     </div>
