8e337a9ecdeeb5d54ea1e803f7cc1610a9f46ae6
tmaslikova <55040075+tmaslikova@users.noreply.github.com>
2021-04-18 02:13:12 -0400

    Lots of tweaks to user creation
    Ensure roles are set properly, and if they signed up with a valid university email go ahead and populate their universityid field correct, etc.

--- html/shared/functions.php
+++ html/shared/functions.php
@@ -38,12 +38,33 @@ function emailExists($conn, $email)
 
     // if theres is info in the data base with this email grab data 
     // assigns data to var row
-    if ($row = mysqli_fetch_assoc($resultData))
+	$row = mysqli_fetch_assoc($resultData);
+	
+    mysqli_stmt_close($stmt);
+    if ($row)
         return $row;
     else 
     	return false;
-    
+}
+
+function getUniversityMatching($conn, $email){
+	$sql = "SELECT UniversityID FROM University WHERE `Domain` = ?;";
+	$stmt = mysqli_stmt_init($conn);
+	if(!mysqli_stmt_prepare($stmt, $sql))
+		return;
+	
+	mysqli_stmt_bind_param($stmt, "s", explode('@', $email)[1]);
+	
+	mysqli_stmt_execute($stmt);
+
+	$data = mysqli_fetch_assoc(mysqli_stmt_get_result($stmt));
     mysqli_stmt_close($stmt);
+	
+	if($data)
+		return $data['UniversityID'];
+	else
+		return "";
+
 }
 
 function createUser($conn, $fname, $lname, $email, $phone, $password_1)
@@ -69,6 +90,22 @@ function createUser($conn, $fname, $lname, $email, $phone, $password_1)
     mysqli_stmt_execute($stmt);
 
     mysqli_stmt_close($stmt);
+	
+	$uid = getUserIdFromEmail($conn, $email);
+	addRole($conn, $uid, "Public");
+	$uni = getUniversityMatching($conn, $email);
+	
+	if($uni){
+		addRole($conn, $uid, "Student");
+		$stmt = mysqli_stmt_init($conn); // this makes code more secure
+
+		// check for any mistakes before running sql code 
+		if (mysqli_stmt_prepare($stmt, "UPDATE Users SET UniversityID=? WHERE UserID=?;")){
+			mysqli_stmt_bind_param($stmt, "ii", $uni, $uid);
+			mysqli_stmt_execute($stmt);
+			mysqli_stmt_close($stmt);
+		}
+	}
 
     header("location: register.php?error=none");
     exit();
@@ -77,6 +114,49 @@ function createUser($conn, $fname, $lname, $email, $phone, $password_1)
 function emptyInputsLogin($email, $password_1)
 {
 	return empty($email) || empty($password_1);
+}
+
+function getUserIdFromEmail($conn, $email){
+	$sql = "SELECT * FROM Users WHERE Email = ?;";
+
+    // prevents the user from entering malicious code into the registration form
+    // prevents code injection
+    $stmt = mysqli_stmt_init($conn); // this makes code more secure
+
+    // check for any mistakes before running sql code 
+    if (!mysqli_stmt_prepare($stmt, $sql))
+		return;
+		
+    mysqli_stmt_bind_param($stmt, "s", $email);
+    mysqli_stmt_execute($stmt);
+
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    // if theres is info in the data base with this email grab data 
+    // assigns data to var row
+	$row = mysqli_fetch_assoc($resultData);
+	
+    mysqli_stmt_close($stmt);
+    if ($row)
+        return $row['UserID'];
+    else 
+    	return false;
+}
+
+function addRole($conn, $userid, $role){
+	$sql = "INSERT INTO Roles (RoleType, UserID) VALUES (?, ?);";
+
+    // prevents the user from entering malicious code into the registration form
+    // prevents code injection
+    $stmt = mysqli_stmt_init($conn); // this makes code more secure
+
+    // check for any mistakes before running sql code 
+    if (!mysqli_stmt_prepare($stmt, $sql))
+		return;
+    
+    mysqli_stmt_bind_param($stmt, "si", $role, $userid);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
 }
 
 function loginUser($conn, $email, $password_1)
@@ -104,7 +184,7 @@ function loginUser($conn, $email, $password_1)
         $_SESSION["userid"] = $userdata["UserID"];
         $_SESSION["fname"] = $userdata["FirstName"];
 
-        header("location: index.php?error=none");
+        header("location: login.php?error=none");
         exit();
     }
 }
